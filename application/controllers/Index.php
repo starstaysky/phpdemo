5fb1058fda712310a471e590e65ccfa855982b05
fuxing <526390949@qq.com>
2019-05-03 22:55:03 +0800

    update

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,7 +1,14 @@
 <?php
 	class IndexController extends Yaf_Controller_Abstract{
 		public function indexAction(){
-			$this->_view->word = "hello world";
+			echo json_encode(
+				[
+					'errno' => 0,
+					'errmsg' => '成功'
+				]
+			);
+			exit;
+			//$this->_view->word = "hello world";
 			//or
 			//$this->getView()->word = "hello world";
 		}
