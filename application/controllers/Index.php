fb0eaa2992726fff95bc4133dbb587daeb3e587a
fuxing <526390949@qq.com>
2019-05-03 22:57:24 +0800

    update

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,6 +1,7 @@
 <?php
 	class IndexController extends Yaf_Controller_Abstract{
 		public function indexAction(){
+			header('Content-Type:application/json');
 			echo json_encode(
 				[
 					'errno' => 0,
